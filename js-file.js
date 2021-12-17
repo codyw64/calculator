e742495cd07128b1807f3035973f1485f5654c3b
Cody <whearcody@gmail.com>
2021-12-17 21:24:41 +0000

    Added some aesthetics to the calculator, as well as doing a little bit of js and html work.

--- js-file.js
+++ js-file.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a ,b) {
     return a / b;
 }
+
+
+
+
+
+const numbers = document.querySelectorAll("#number");
+numbers.forEach(number => {
+    number.addEventListener('click', (e)=> {
+    console.log(e.target.value);
+    });
+});
+
+const zeros = document.querySelector('#zero');
+zeros.addEventListener('click', (e)=> {
+    console.log(e.target.value);
+});
+
